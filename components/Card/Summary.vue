53ab533995d0bb68ba4da663dd3a439207228db5
mies47 <mies@aut.ac.ir>
2023-06-19 15:53:55 +0330

    feat: modify dashboard and summary to show nodes and gateways

--- components/Card/Summary.vue
+++ components/Card/Summary.vue
@@ -1,25 +1,19 @@
 <template>
-  <v-row v-if="items && items.length">
-    <v-col
-      v-for="item in items"
-      :key="item.title.replace(' ', '_')"
-      cols="6"
-      sm="3"
-    >
+  <v-row v-if="items && Object.entries(items).length">
+    <v-col v-for="[k, v] of Object.entries(items)" :key="k" cols="6" sm="4">
       <v-card height="100%" class="card">
         <v-container fluid>
-          <h3>{{ item.title }}</h3>
+          <h3>{{ v.title }}</h3>
           <v-row height="100%">
             <v-col>
               <h4
                 class="grey--text text-h4 text-lg-h2 font-weight-bold lh-normal"
               >
-                {{ item.summary }}
+                {{ v.summary }}
               </h4>
-              <h5 class="font-weight-normal grey--text">{{ item.total }}</h5>
             </v-col>
             <v-col align-self="center" align="center">
-              <v-icon size="60" :color="item.color">{{ item.icon }}</v-icon>
+              <v-icon size="60" :color="v.color">{{ v.icon }}</v-icon>
             </v-col>
           </v-row>
         </v-container>
@@ -31,38 +25,10 @@
 <script>
 export default {
   name: 'CardSummary',
-  data() {
-    return {
-      items: [
-        {
-          title: 'Users Summary',
-          summary: '5230',
-          total: '+23% Up',
-          color: 'green',
-          icon: 'mdi-account-box-outline'
-        },
-        {
-          title: 'Sales Summary',
-          summary: '6424',
-          total: '-3% Down',
-          color: 'warning',
-          icon: 'mdi-cart-plus'
-        },
-        {
-          title: 'Stock Summary',
-          summary: '3920',
-          total: '-10% Down',
-          color: 'red',
-          icon: 'mdi-chart-areaspline'
-        },
-        {
-          title: 'Views Summary',
-          summary: '4124',
-          total: '+50% Up',
-          color: 'orange',
-          icon: 'mdi-gesture-double-tap'
-        }
-      ]
+  props: {
+    items: {
+      type: Object,
+      required: true
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -2,237 +2,77 @@
   <v-container fluid>
     <v-row>
       <v-col cols="12" md="12">
-        <h1 class="title-page">Hi, Welcome to Nuxtify</h1>
-        <CardSummary />
-        <v-row>
-          <v-col cols="12" lg="4">
-            <v-card height="100%">
-              <v-card-title>Sales by brand</v-card-title>
-              <v-card-text>
-                <p>
-                  Our daily corporate brand sales summaries in wholesale. This
-                  summary is active from 00.00 to 24.00 monthly.
-                </p>
-                <client-only placeholder="Loading...">
-                  <DoughnutChart
-                    :chart-data="doughChartData"
-                    :chart-options="doughChartOptions"
-                    :height="430"
-                  />
-                </client-only>
-                <v-row>
-                  <v-col cols="9">
-                    <p>
-                      The sales summary is done by our company on salesperson
-                      activities based on the brand they sell daily on a monthly
-                      basis.
-                    </p></v-col
-                  >
-                  <v-col cols="3">
-                    <v-btn type="submit" color="primary" small tile
-                      >See all</v-btn
-                    ></v-col
-                  ></v-row
-                >
-              </v-card-text>
+        <CardSummary :items="summary" />
+        <h2 class="my-5">Active Nodes</h2>
+        <v-row style="min-height: 20vh">
+          <v-col v-for="node in nodes" :key="node" cols="12" sm="6" lg="4">
+            <v-card height="100%" class="card" :to="`/node/${node}`">
+              <v-container fluid>
+                <v-row height="100%">
+                  <v-col cols="9" align-self="center" align="center">
+                    <div>{{ node }}</div>
+                  </v-col>
+                  <v-col cols="3" align-self="center" align="center">
+                    <v-icon size="60" color="red">mdi-vibrate</v-icon>
+                  </v-col>
+                </v-row>
+              </v-container>
             </v-card>
           </v-col>
-          <v-col cols="12" lg="8">
-            <v-card height="100%" light>
-              <v-card-title>Person sales by month</v-card-title>
-              <v-card-text>
-                <p>
-                  The sales summary is done by our company on salesperson
-                  activities based on the brand they sell daily on a monthly
-                  basis. These are the top three sales by a person on average
-                  above 5 sales monthly.
-                </p>
-                <!-- <v-col cols="3">
-                      <v-select
-                        :items="[
-                          'Division 1',
-                          'Division 2',
-                          'Division 3',
-                          'Division 4'
-                        ]"
-                        label="Division"
-                        dense
-                        outlined
-                      ></v-select>
-                    </v-col> -->
-                <client-only placeholder="Loading...">
-                  <LineChart
-                    :chart-options="lineChartOptions"
-                    :chart-data="lineChartData"
-                    :chart-id="`chartId`"
-                    :width="300"
-                    :height="400"
-                    :styles="{
-                      margin: '1rem auto',
-                      backgroundColor: 'rgba(255,255,255, .85)',
-                      padding: '.75rem'
-                    }"
-                  />
-                </client-only>
-                <p>
-                  The performance of the sales summary will start from the start
-                  of the year to the end year and will impact the person's
-                  yearly performance review, and bonus rewards.
-                </p>
-              </v-card-text>
-            </v-card>
+          <v-col
+            v-if="nodes.length == 0"
+            cols="12"
+            align-self="center"
+            class="text-center grey--text"
+          >
+            There are currently no active nodes or an error has occured!
           </v-col>
         </v-row>
-        <v-row>
-          <v-col cols="12" md="8">
-            <v-card height="100%" class="card">
+        <h2 class="my-5">Active Gateways</h2>
+        <v-row style="min-height: 20vh">
+          <v-col
+            v-for="gateway in gateways"
+            :key="gateway.mac"
+            cols="12"
+            sm="6"
+            lg="4"
+          >
+            <v-card height="100%" class="card" :to="`/gateway/${gateway.mac}`">
               <v-container fluid>
-                <h3 class="title-page">
-                  Company official yearly calendar release
-                </h3>
-                <div>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
-                  nihil corporis rem iste! Doloribus magni doloremque inventore
-                  natus consectetur corporis debitis nemo numquam explicabo esse
-                  iusto maxime.
-                </div>
-                <v-calendar class="my-6"></v-calendar>
+                <v-row height="100%">
+                  <v-col cols="9" align-self="center" align="center">
+                    <div>MAC: {{ gateway.mac }}</div>
+                    <div>
+                      Verified:
+                      <v-icon
+                        small
+                        :color="gateway.is_verified ? 'green' : 'red'"
+                      >
+                        {{ gateway.is_verified ? 'mdi-check' : 'mdi-close' }}
+                      </v-icon>
+                    </div>
+                  </v-col>
+                  <v-col cols="3" align-self="center" align="center">
+                    <v-icon size="60" color="blue">mdi-router-wireless</v-icon>
+                  </v-col>
+                </v-row>
               </v-container>
             </v-card>
           </v-col>
-          <v-col cols="12" md="4">
-            <v-card height="100%" class="card">
-              <v-container>
-                <h3 class="title-page">Company News Announcement</h3>
-                <div>
-                  Here are the latest announcement notifications across our
-                  company! So you do not have to fear missing out on the latest
-                  news
-                </div>
-                <v-container fluid>
-                  <!-- <v-virtual-scroll width="256" height="321" item-height="120"> -->
-                  <v-row no-gutters>
-                    <v-col cols="2" align-self="center" align="center">
-                      <v-avatar color="primary">
-                        <span class="white--text font-weight-bold">AS</span>
-                      </v-avatar>
-                    </v-col>
-                    <v-col cols="10">
-                      <blockquote class="blockquote grey--text body-1 px-3">
-                        <div class="mb-1">
-                          Fugit nihil corporis rem iste! Doloribus magni
-                          doloremque
-                        </div>
-                        <cite class="font-weight-thin"
-                          >- Any Sanders. Sr Marketing Director</cite
-                        >
-                      </blockquote>
-                    </v-col>
-                  </v-row>
-                  <v-row no-gutters>
-                    <v-col cols="9">
-                      <blockquote
-                        class="blockquote grey--text body-1 px-3 text-right"
-                      >
-                        <div class="mb-1">
-                          Fugit nihil corporis rem iste! Doloremque inventore
-                          natus nemo
-                        </div>
-                        <cite class="font-weight-thin"
-                          >- Carol Violas. Sales Representative</cite
-                        >
-                      </blockquote>
-                    </v-col>
-                    <v-col cols="3" align-self="center" align="center">
-                      <v-avatar color="green">
-                        <span class="white--text font-weight-bold">CV</span>
-                      </v-avatar>
-                    </v-col>
-                  </v-row>
-                  <v-row no-gutters>
-                    <v-col cols="2" align-self="center" align="center">
-                      <v-avatar color="info">
-                        <span class="white--text font-weight-bold">NM</span>
-                      </v-avatar>
-                    </v-col>
-                    <v-col cols="10">
-                      <blockquote class="blockquote grey--text body-1 px-3">
-                        <div class="mb-1">
-                          Fugit nihil corporis rem iste! Doloribus natus
-                          consectetur
-                        </div>
-                        <cite class="font-weight-thin"
-                          >- Naomi Malone. Country Director</cite
-                        >
-                      </blockquote>
-                    </v-col>
-                  </v-row>
-                  <!-- <v-row no-gutters>
-                      <v-col cols="9">
-                        <blockquote
-                          class="blockquote grey--text body-1 px-3 text-right"
-                        >
-                          <div class="mb-1">
-                            Fugit nihil corporis rem iste! Doloremque inventore
-                            natus nemo
-                          </div>
-                          <cite class="font-weight-thin"
-                            >- Carol Violas. Sales Representative</cite
-                          >
-                        </blockquote>
-                      </v-col>
-                      <v-col cols="3" align-self="center" align="center">
-                        <v-avatar color="green">
-                          <span class="white--text font-weight-bold">CV</span>
-                        </v-avatar>
-                      </v-col>
-                    </v-row>
-                    <v-row no-gutters>
-                      <v-col cols="2" align-self="center" align="center">
-                        <v-avatar color="primary">
-                          <span class="white--text font-weight-bold">AS</span>
-                        </v-avatar>
-                      </v-col>
-                      <v-col cols="10">
-                        <blockquote class="blockquote grey--text body-1 px-3">
-                          <div class="mb-1">
-                            Fugit nihil corporis rem iste! Doloribus magni
-                            doloremque
-                          </div>
-                          <cite class="font-weight-thin"
-                            >- Any Sanders. Sr Marketing Director</cite
-                          >
-                        </blockquote>
-                      </v-col>
-                    </v-row>
-                    <v-row no-gutters>
-                      <v-col cols="9">
-                        <blockquote
-                          class="blockquote grey--text body-1 px-3 text-right"
-                        >
-                          <div class="mb-1">
-                            Fugit nihil corporis rem iste! Doloremque inventore
-                            natus nemo
-                          </div>
-                          <cite class="font-weight-thin"
-                            >- Carol Violas. Sales Representative</cite
-                          >
-                        </blockquote>
-                      </v-col>
-                      <v-col cols="3" align-self="center" align="center">
-                        <v-avatar color="green">
-                          <span class="white--text font-weight-bold">CV</span>
-                        </v-avatar>
-                      </v-col>
-                    </v-row> -->
-                  <!-- </v-virtual-scroll> -->
-                </v-container>
-              </v-container>
-            </v-card>
+          <v-col
+            v-if="gateways.length == 0"
+            cols="12"
+            align-self="center"
+            class="text-center grey--text"
+          >
+            There are currently no active gateways or an error has occured!
           </v-col>
         </v-row>
       </v-col>
     </v-row>
+    <v-overlay :value="loading">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
     <Footer />
   </v-container>
 </template>
@@ -246,89 +86,67 @@ export default {
   },
   data() {
     return {
-      doughChartData: {
-        labels: ['NuVue', 'VueJit', 'IftShi', 'KoinVi', 'MegsWear', 'Millgh'],
-        datasets: [
-          {
-            label: 'Visualization',
-            data: [72, 131, 12, 3, 4, 55],
-            backgroundColor: [
-              'rgba(20, 255, 0, 0.85)',
-              'rgba(200, 5, 0, 0.85)',
-              'rgba(10, 220, 0, 0.85)',
-              'rgba(2, 100, 0, 0.85)',
-              'rgba(20, 55, 0, 0.85)',
-              'rgba(120, 155, 0, 0.85)'
-            ],
-            borderColor: 'rgba(100, 155, 0, 1)',
-            borderWidth: 0
-          }
-        ]
+      summary: {
+        admin: {
+          title: 'Admins Summary',
+          summary: 0,
+          color: 'green',
+          icon: 'mdi-account-box-outline'
+        },
+        gateway: {
+          title: 'Gateway Summary',
+          summary: 0,
+          color: 'blue',
+          icon: 'mdi-router-wireless'
+        },
+        node: {
+          title: 'Nodes Summary',
+          summary: 0,
+          color: 'red',
+          icon: 'mdi-vibrate'
+        }
       },
-      doughChartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-        offset: 8,
-        radius: 160,
-        spacing: 4,
-        hoverOffset: 32,
-        hoverBorderWidth: 1,
-        weight: 0
-      },
-      lineChartData: {
-        labels: [
-          'Jan',
-          'Feb',
-          'Mar',
-          'Apr',
-          'May',
-          'Jun',
-          'Jul',
-          'Aug',
-          'Sep',
-          'Oct',
-          'Nov',
-          'Dec'
-        ],
-        datasets: [
-          {
-            label: 'John Doe',
-            backgroundColor: '#ff0099',
-            borderColor: '#ff0099',
-            borderWidth: '0',
-            borderJoinStyle: 'round',
-            data: [40, 39, 10, 40, 39, 80, 40, 30, 10, 20, 30, 40]
-          },
-          {
-            label: 'Jane Doe',
-            backgroundColor: '#ff9900',
-            borderColor: '#ff9900',
-            borderWidth: '0',
-            borderJoinStyle: 'round',
-            data: [20, 9, 80, 90, 29, 58, 80, 20, 30, 40, 55, 75]
-          },
-          {
-            label: 'Jack Doe',
-            backgroundColor: '#999999',
-            borderColor: '#999999',
-            borderWidth: '0',
-            borderJoinStyle: 'round',
-            data: [10, 24, 42, 50, 69, 10, 20, 70, 89, 45, 35, 42]
-          }
-        ]
-      },
-      lineChartOptions: {
-        responsive: true,
-        maintainAspectRatio: false
-      }
+      loading: true,
+      nodes: [],
+      gateways: []
     }
   },
   head() {
     return {
-      title: 'Homepage - Nuxtify Admin Dashboard Template by dykraf.com',
-      description:
-        'A Collection of Templates in Nuxtify Admin Template Dashboard by dykraf.com'
+      title: 'Homepage - Admin Dashboard',
+      description: 'An admin dashboard for predictive maintenance analytics.'
     }
+  },
+  mounted() {
+    this.$axios
+      .$get('/admin/all')
+      .then((data) => {
+        this.summary.admin.summary = data.length
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    this.$axios
+      .$get('/gateway/all')
+      .then((data) => {
+        this.summary.gateway.summary = data.length
+        this.gateways = data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    this.$axios
+      .$get('/data/allNodes')
+      .then((data) => {
+        this.summary.node.summary = data.length
+        this.nodes = data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+      .finally(() => {
+        this.loading = false
+      })
   }
 }
 </script>
